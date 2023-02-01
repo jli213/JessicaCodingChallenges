39d2d14a790acf287af0e3fb21136f852e94e3d4
JessicaLI <jessica.li1@ucalgary.ca>
2023-02-01 07:07:52 +0000

    Add error label for project grade.

--- eclipse-workspace/JessicaLiGradeCalculator/src/application/GradeCalculatorController.java
+++ eclipse-workspace/JessicaLiGradeCalculator/src/application/GradeCalculatorController.java
@@ -24,6 +24,9 @@ public class GradeCalculatorController {
     
     @FXML
     private Label courseGradeLabel;
+    
+    @FXML
+    private Label projectErrorLabel;
 
     @FXML
     void calculateGrade(ActionEvent event) {
