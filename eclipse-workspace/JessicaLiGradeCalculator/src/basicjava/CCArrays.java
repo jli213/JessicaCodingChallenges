29a8ee46a7de159f55be8bedfb5ef7fdcb582b36
CS219-user <CS219-user@10.0.0.220>
2023-02-24 04:25:17 +0000

    replace method test are all passed

--- eclipse-workspace/JessicaLiGradeCalculator/src/basicjava/CCArrays.java
+++ eclipse-workspace/JessicaLiGradeCalculator/src/basicjava/CCArrays.java
@@ -1,10 +1,20 @@
 package basicjava;
 
+import java.util.Arrays;
+
 public class CCArrays {
 
 	public static void replace(char[] chars, char c, char d) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub	
 		
+		int length = chars.length;
+		for(int i=0;i<length;i++) {
+			System.out.println("before "+chars[i]);
+			if(chars[i] == c || chars[i] == (char)c+32|| chars[i] ==(char) c-32){
+				chars[i] = d;
+				System.out.println("after "+chars[i]);
+			}
+		}
 	}
 
 	public static void sortAlphabetic(String[] strs) {
