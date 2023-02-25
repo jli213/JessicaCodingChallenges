621d253fe83ddffb4065f6ba6af60d10c42137e6
CS219-user <CS219-user@10.0.0.220>
2023-02-25 06:58:36 +0000

    finished creating class for coding challenge

---
+++ eclipse-workspace/JessicaLiGradeCalculator/src/basicjava/CCArrayList.java
@@ -0,0 +1,18 @@
+package basicjava;
+
+import java.util.ArrayList;
+
+public class CCArrayList {
+
+	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void insert(ArrayList<Double> nums, double d, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
