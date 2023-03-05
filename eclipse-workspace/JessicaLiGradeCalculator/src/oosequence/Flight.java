76ee2c6e667a2048ad7a08f09e8f43875fc9a9ce
CS219-user <CS219-user@lab000000.cqdhyqslc1pedihwldg3213dfg.ux.internal.cloudapp.net>
2023-03-05 16:34:03 +0000

    completed skeleton of Flight class

---
+++ eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Flight.java
@@ -0,0 +1,23 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+
+	public Date departure;
+	public Date arrival;
+
+	public Flight(Date departureTime, Date arrivalTime) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Flight(Flight c) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
