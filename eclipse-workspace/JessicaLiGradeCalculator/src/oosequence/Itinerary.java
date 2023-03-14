350140f7f329025b2f33c7aa0419a7031200db56
CS219-user <CS219-user@10.0.0.220>
2023-03-14 23:52:46 +0000

    working on addFlight function

--- eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Itinerary.java
+++ eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Itinerary.java
@@ -3,19 +3,46 @@ package oosequence;
 import java.util.ArrayList;
 
 public class Itinerary {
+	
+	private ArrayList<Flight> flights;
+	private String name = null;
 
-	public Itinerary(String string) {
+	public Itinerary(String nameString) {
+		name = nameString;
+		flights = new ArrayList<Flight>();
 		// TODO Auto-generated constructor stub
 	}
 
-	public void addFlight(Flight m) {
+	public void addFlight(Flight flightToAdd) {
 		// TODO Auto-generated method stub
+		int size = flights.size();
+		if(flights.isEmpty()) {
+			flights.add(flightToAdd);
+		}
+		else {
+			for(int j=0;j<size;j++) {
+				if(j==size-1){
+					if (flightToAdd.getArrival().after(flights.get(j).getArrival())) {
+						if(flightToAdd.getDeparture().after(flights.get(j).getArrival())){
+							flights.add(j, flightToAdd);
+						}
+					}
+					if (flightToAdd.getArrival().after(flights.get(j).getArrival())) {
+						if(flightToAdd.getDeparture().after(flights.get(j).getArrival())){
+							flights.add(j, flightToAdd);
+						}
+					}
+				}
+				else if(flights.get(j).getArrival().after(flightToAdd.getArrival())) {
+					if(flights.get(j-1).getDeparture().after(flightToAdd.getArrival())){
+						if(flightToAdd.getDeparture().after(flights.get(j).getArrival())) {
+							flights.add(j, flightToAdd);
+						}
+					} 
+				}
+			}
+		}
 		
-	}
-
-	public ArrayList<Flight> getFlights() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public long getTotalLayover() {
@@ -23,11 +50,15 @@ public class Itinerary {
 		return 0;
 	}
 
-	public Object getName() {
+	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
-
+	
+	public ArrayList<Flight> getFlights() {
+		// TODO Auto-generated method stub
+		return flights;
+	}
 	
 
 }
