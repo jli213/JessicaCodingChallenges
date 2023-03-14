da39e4eed13091bb17026264c5dad33d6cacae69
CS219-user <CS219-user@10.0.0.220>
2023-03-14 22:53:54 +0000

    finished flight class

--- eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Flight.java
+++ eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Flight.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class Flight {
 
-	public Date departure;
-	public Date arrival;
+	private Date departure = null;
+	private Date arrival = null;
 
 	public Flight(Date departureTime, Date arrivalTime) {
 		if(departureTime == null || arrivalTime == null) {
@@ -38,6 +38,40 @@ public class Flight {
 			time = arrival.getTime() - departure.getTime();
 		}
 		return time/60000;
+	}
+
+	public Date getDeparture() {
+		// TODO Auto-generated method stub
+		return departure;
+	}
+
+	public Date getArrival() {
+		// TODO Auto-generated method stub
+		return arrival;
+	}
+	
+	public void setDeparture(Date departureTime) {
+		// TODO Auto-generated method stub
+		if(departureTime == null ||arrival == null) {
+			departure = departureTime;
+		}
+		else {
+			if(departureTime.before(arrival)) {
+				departure = departureTime;
+			}
+		}
+	}
+
+	public void setArrival(Date arrivalTime) {
+		// TODO Auto-generated method stub
+		if(arrivalTime == null || departure == null) {
+			arrival = arrivalTime;
+		}
+		else {
+			if(departure.before(arrivalTime)) {
+				arrival = arrivalTime;
+			}
+		}
 	}
 
 }
---
+++ eclipse-workspace/JessicaLiGradeCalculator/src/oosequence/Itinerary.java
@@ -0,0 +1,33 @@
+package oosequence;
+
+import java.util.ArrayList;
+
+public class Itinerary {
+
+	public Itinerary(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addFlight(Flight m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Flight> getFlights() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getTotalLayover() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
